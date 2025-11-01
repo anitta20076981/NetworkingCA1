dc087efa81da8dc204da9a7e4ead88f685aaa9b6
anitta20076981 <anitta.antony06@gmail.com>
2025-11-01 12:25:25 +0000

    Add Terraform EKS and ECR setup with existing resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,80 +11,38 @@ provider "aws" {
   region = var.aws_region
 }
 
-# -------------------- VPC + SUBNETS --------------------
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "terraform-vpc" }
+# -------------------- USE EXISTING VPC + SUBNETS --------------------
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
 # -------------------- IAM ROLE FOR EKS --------------------
-data "aws_iam_policy_document" "eks_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name               = "terraform-eks-cluster-role-2"   # unique name
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "vpc_cni_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+data "aws_iam_role" "eks_cluster_role" {
+  name = "terraform-eks-cluster-role-2" # existing role
 }
 
 # -------------------- EKS CLUSTER --------------------
 resource "aws_eks_cluster" "eks" {
-  name     = "terraform-eks-cluster-2"   # unique cluster name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  name     = "terraform-eks-cluster-2"
+  role_arn = data.aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.public[*].id
+    subnet_ids = data.aws_subnets.default.ids
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.cluster_policy,
-    aws_iam_role_policy_attachment.vpc_cni_policy
-  ]
 }
 
 # -------------------- ECR REPOSITORY --------------------
-resource "aws_ecr_repository" "app" {
-  name                 = "my-simple-app-3"   # unique repo name
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "app" {
+  name = "my-simple-app-3" # existing repository
 }
 
 # -------------------- OUTPUTS --------------------
@@ -93,5 +51,5 @@ output "eks_cluster_name" {
 }
 
 output "ecr_repository_uri" {
-  value = aws_ecr_repository.app.repository_url
+  value = data.aws_ecr_repository.app.repository_url
 }
